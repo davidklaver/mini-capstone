f7429061cc6ff55ed55a4e09e9049681519cd9e2
davidklaver <davidjklaver@gmail.comssh-keygen -t rsa -C davidjklaver@gmail.com>
2016-11-08 21:23:49 -0500

    Use faker to generate another 100 products with placeholder images, and add some formatting using css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+	product = Product.new(name: Faker::Commerce.product_name, price: Faker::Commerce.price, image: "image not available", description: Faker::Commerce.color)
+	product.save
+end
